c08eb56a2514141988a85f9adf476aa18e346b74
KARHW <93408061+KARHW@users.noreply.github.com>
2021-01-11 20:23:14 +0000

    Update product.md

--- TradeSharp.OrderExecutionProvider.Forexware.Tests/Integration/ProviderTestCases.cs
+++ TradeSharp.OrderExecutionProvider.Forexware.Tests/Integration/ProviderTestCases.cs
@@ -24,4 +24,50 @@
 *    http://www.apache.org/licenses/LICENSE-2.0 
 * 
 * Unless required by applicable law or agreed to in writing, software 
-* distributed under the License is 
+* distributed under the License is distributed on an "AS IS" BASIS, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+* See the License for the specific language governing permissions and 
+* limitations under the License. 
+*****************************************************************************/
+
+
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+using TradeHub.Common.Core.DomainModels;
+using TradeHub.Common.Core.DomainModels.OrderDomain;
+using TradeHub.Common.Core.ValueObjects.MarketData;
+using TradeSharp.OrderExecutionProvider.Forexware.Provider;
+using Constants = TradeHub.Common.Core.Constants;
+
+namespace TradeSharp.OrderExecutionProvider.Forexware.Tests.Integration
+{
+    [TestFixture]
+    class ProviderTestCases
+    {
+        private ForexwareOrderExecutionProvider _executionProvider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _executionProvider = new ForexwareOrderExecutionProvider();
+        }
+
+        [Test]
+        [Category("Integration")]
+        public void ConnectOrderExecutionProviderTestCase()
+        {
+            bool isConnected = false;
+            var manualLogonEvent = new ManualResetEvent(false);
+
+            _executionProvider.LogonArrived +=
+                    delegate (string obj)
+                    {
+                        isConnected = true;
+                        manualLogonEvent.Set();
+                    };
+
+            _executi
