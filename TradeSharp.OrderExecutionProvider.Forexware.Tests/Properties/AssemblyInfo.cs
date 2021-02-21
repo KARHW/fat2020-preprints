0db77aab65ee78eb1d07c1d4f62c9c391dcd1c9f
KARHW <93408061+KARHW@users.noreply.github.com>
2021-02-21 21:48:33 +0000

    merged readme

--- TradeSharp.OrderExecutionProvider.Forexware.Tests/Properties/AssemblyInfo.cs
+++ TradeSharp.OrderExecutionProvider.Forexware.Tests/Properties/AssemblyInfo.cs
@@ -52,4 +52,18 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The follo
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3682fb56-d60f-473b-a8ca-8f04be55b867")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
